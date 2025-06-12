9d71aec521e0a7d58069eea146a289318629453e
crashmirage <128055064+crashmirage@users.noreply.github.com>
2025-06-12 18:36:53 +0200

    Update main_MVD_ARIA.py

--- main_MVD_ARIA.py
+++ main_MVD_ARIA.py
@@ -113,7 +113,7 @@ def scrape_epreuve(epreuve: str):
         service=Service(ChromeDriverManager().install()),    
         options=options,
         browser_executable_path=chrome_path,
-        patcher_executable_path=chrome,
+        patcher_executable_path=chrome_path,
         use_subprocess=True)
 
     url = f"https://www.atletiek.nu/ranglijst/belgische-ranglijst/2025/outdoor/scholieren-jongens/{epreuve}/"
